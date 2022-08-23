747d34b903ad6e475c9bc32c6ce55afb3c381d5a
Steven Dickenson <sdickenson@rhythmictech.com>
2022-08-23 17:32:21 -0400

    Remove template provider

--- asg.tf
+++ asg.tf
@@ -5,27 +5,20 @@
 # cloudformations support for rolling updates
 ########################################
 
-data "template_file" "asg_cfn" {
-  count = var.max_instances < 1 ? 0 : 1
-
-  template = file("${path.module}/asg.cfn.yml.tpl")
-
-  vars = {
-    description     = "Autoscaling group for ECS cluster"
-    desiredCapacity = var.desired_instances
-    healthCheck     = var.asg_health_check_type
-    launchConfig    = try(aws_launch_configuration.this[0].name, "")
-    maxSize         = var.max_instances
-    maxBatch        = var.asg_max_size
-    minInService    = var.max_instances / 2
-    minSize         = var.min_instances
-    subnets         = join("\",\"", var.ec2_subnet_ids)
-  }
-}
-
 resource "aws_cloudformation_stack" "ecs_asg" {
   count = var.max_instances < 1 ? 0 : 1
 
-  name          = "${regex("[a-zA-Z][-a-zA-Z0-9]*", var.name)}-asg-stack"
-  template_body = try(data.template_file.asg_cfn[0].rendered, "")
+  name = "${regex("[a-zA-Z][-a-zA-Z0-9]*", var.name)}-asg-stack"
+  template_body = try(templatefile("${path.module}/asg.cfn.yml.tpl",
+    {
+      description     = "Autoscaling group for ECS cluster"
+      desiredCapacity = var.desired_instances
+      healthCheck     = var.asg_health_check_type
+      launchConfig    = try(aws_launch_configuration.this[0].name, "")
+      maxSize         = var.max_instances
+      maxBatch        = var.asg_max_size
+      minInService    = var.max_instances / 2
+      minSize         = var.min_instances
+      subnets         = join("\",\"", var.ec2_subnet_ids)
+  }), "")
 }
